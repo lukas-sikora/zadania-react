7205035e936dffe0b38ff84bb86ab400a5557286
RafalKielbasa <kielbasarafal92@gmail.com>
2024-03-07 23:41:42 +0100

    props-task/1-solution

--- react-training/src/components/DownloadButton.jsx
+++ react-training/src/components/DownloadButton.jsx
@@ -2,8 +2,14 @@ export function DownloadButton({ isDocumentAvailable }) {
   const documentUrl = '<https://example.com/document.pdf>'
 
   return (
-    <button disabled={!isDocumentAvailable}>
-      {isDocumentAvailable ? 'Pobierz dokument' : 'Dokument niedostępny'}
-    </button>
+    <a
+      href={isDocumentAvailable ? documentUrl : '#'}
+      download={isDocumentAvailable}
+      style={{ pointerEvents: isDocumentAvailable ? 'auto' : 'none' }}
+    >
+      <button disabled={!isDocumentAvailable}>
+        {isDocumentAvailable ? 'Pobierz dokument' : 'Dokument niedostępny'}
+      </button>
+    </a>
   )
 }
--- react-training/src/main.jsx
+++ react-training/src/main.jsx
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom/client'
 import { DownloadButton } from './components/DownloadButton'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <DownloadButton isDocumentAvailable={true} />
+  <DownloadButton isDocumentAvailable={false} />
 )
