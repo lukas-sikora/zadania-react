d878e63af9b483ab5b2b30c205ecc921447e59e6
RafalKielbasa <kielbasarafal92@gmail.com>
2024-03-07 17:20:38 +0100

    components-task/2

--- react-training/src/main.jsx
+++ react-training/src/main.jsx
@@ -1,5 +1,23 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const WeatherWidget = () => {
+  return (
+    <div>
+      <h2>Prognoza pogody</h2>
+      <p>
+        <strong>Lublin: 25°C,</strong> słonecznie
+      </p>
+      <p>
+        <strong>Warszawa: 20°C,</strong> deszczowo
+      </p>
+      <p>
+        <strong>Poznań: 24°C,</strong> słonecznie
+      </p>
+      <p>
+        <strong>Wrocław: 22°C,</strong> częściowo zachmurzone
+      </p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<WeatherWidget />)
