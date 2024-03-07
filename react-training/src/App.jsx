66b9868a543baa769d11a1848c5150fc910dccce
RafalKielbasa <kielbasarafal92@gmail.com>
2024-03-07 14:56:20 +0100

    what-is-jsx-task/3-solution

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,11 +1,15 @@
 const currentDate = new Date()
-const hours = currentDate.getHours()
-const minutes = currentDate.getMinutes()
 
-const App = () => (
-  <h1>
-    {hours}:{minutes}
-  </h1>
-)
+const App = () => {
+  const hours = String(currentDate.getHours()).padStart(2, '0')
+  const minutes = String(currentDate.getMinutes()).padStart(2, '0')
+  const seconds = String(currentDate.getSeconds()).padStart(2, '0')
+
+  return (
+    <h1>
+      {hours}:{minutes}:{seconds}
+    </h1>
+  )
+}
 
 export default App
