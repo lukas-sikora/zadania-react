c4ce935ac5de2c712ddd512027be186d31187353
RafalKielbasa <kielbasarafal92@gmail.com>
2024-03-07 13:48:00 +0100

    initial-commit

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,17 @@
 function App() {
-  return <div></div>
+  return (
+
+      <div>
+        <h1 class='header'>Witaj w React!</h1>
+        <p>To jest paragraf w React.</p>
+        <div>
+          <span>React jest </span>
+          <span> niesamowity.</span>
+        </div>
+      </div>
+      <footer>Stopka</footer>
+   
+  )
 }
 
 export default App
