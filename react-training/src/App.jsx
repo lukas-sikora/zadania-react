724c933ce9bd306a5790cff3b28e4b7a0b859e20
RafalKielbasa <kielbasarafal92@gmail.com>
2024-03-07 14:40:40 +0100

    what-is-jsx-task/2-solution

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,13 +1,11 @@
 const App = () => {
-	
-  const header = "Witaj w React"
-  
-    return (
-      <div>
-        <h1 className="header">{"header"}</h1>
-        <div>
-      </div>
-    );
-  };
-  
-  export default App;
+  const header = 'Witaj w React'
+
+  return (
+    <div>
+      <h1 className='header'>{header}</h1>
+    </div>
+  )
+}
+
+export default App
