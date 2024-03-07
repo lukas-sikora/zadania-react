3745902fced74e66f943b01fd58c81b6f0d51b5c
RafalKielbasa <kielbasarafal92@gmail.com>
2024-03-07 14:50:36 +0100

    what-is-jsx-task/3

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-  return <div></div>
-}
+const currentDate = new Date()
+const hours = currentDate.getHours()
+const minutes = currentDate.getMinutes()
+
+const App = () => (
+  <h1>
+    {hours}:{minutes}
+  </h1>
+)
 
 export default App
