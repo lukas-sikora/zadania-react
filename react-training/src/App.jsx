1735fa3e020babe640ab30e3ccaad81a6866390d
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-08 20:25:53 +0100

    conditional-rendering-task/2

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,22 @@
-function App() {
-  return <div></div>
-}
+import { UserProfile } from "./components/UserProfile";
 
-export default App
+const user1 = {
+  name: "Marta",
+  age: 29,
+  occupation: "Inżynier oprogramowania",
+};
+const user2 = { name: "Tomasz", age: 34 };
+const user3 = { name: "Ewa", age: 27, occupation: "Projektant UX" };
+const user4 = { name: "Piotr", age: 31, occupation: "Tester oprogramowania" };
+
+const App = () => {
+  return (
+    <>
+      <UserProfile user={user1} />
+      <UserProfile user={user2} />
+      <UserProfile user={user3} />
+      <UserProfile user={user4} />
+    </>
+  );
+};
+export default App;
