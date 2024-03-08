9997de8c94818ca635c88f8d75f49e150973a519
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-08 21:54:55 +0100

    use-state-task/3-solution

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,22 +1,33 @@
+import { useState } from "react";
+
 const App = () => {
+  const [activeSection, setActiveSection] = useState("Mars");
+
   return (
     <>
       <h1>Wspaniałe fakty o planetach</h1>
-      <button>Mars</button>
-      <button>Jowisz</button>
-      <button>Saturn</button>
-      <section>
-        <h2>Mars</h2>
-        <p>Mars jest znaną jako Czerwona Planeta.</p>
-      </section>
-      <section>
-        <h2>Jowisz</h2>
-        <p>Jowisz jest największą planetą w naszym układzie słonecznym.</p>
-      </section>
-      <section>
-        <h2>Saturn</h2>
-        <p>Saturn słynie z pięknych pierścieni.</p>
-      </section>
+      <button onClick={() => setActiveSection("Mars")}>Mars</button>
+      <button onClick={() => setActiveSection("Jowisz")}>Jowisz</button>
+      <button onClick={() => setActiveSection("Saturn")}>Saturn</button>
+
+      {activeSection === "Mars" && (
+        <section>
+          <h2>Mars</h2>
+          <p>Mars jest znaną jako Czerwona Planeta.</p>
+        </section>
+      )}
+      {activeSection === "Jowisz" && (
+        <section>
+          <h2>Jowisz</h2>
+          <p>Jowisz jest największą planetą w naszym układzie słonecznym.</p>
+        </section>
+      )}
+      {activeSection === "Saturn" && (
+        <section>
+          <h2>Saturn</h2>
+          <p>Saturn słynie z pięknych pierścieni.</p>
+        </section>
+      )}
     </>
   );
 };
