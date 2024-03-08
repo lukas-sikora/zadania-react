bb49925443cd8eefb9bb7d87c7891aeb7ac8feee
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-08 21:16:18 +0100

    use-state-task/1-solution

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => setIsDarkMode(!isDarkMode);
+
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <h1>Tryb ciemny</h1>
-      <button>Zmień tryb</button>
+    <div
+      style={{
+        backgroundColor: isDarkMode ? "#333" : "#FFF",
+        color: isDarkMode ? "#FFF" : "#000",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <h1>{isDarkMode ? "Tryb ciemny" : "Tryb jasny"}</h1>
+      <button onClick={toggleDarkMode}>
+        {isDarkMode ? "Wyłącz tryb ciemny" : "Włącz tryb ciemny"}
+      </button>
     </div>
   );
 };
