5be8958ba116ee7e20b31245dea6d2e6f5b5e955
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-08 16:17:44 +0100

    props-task/3-solution

--- react-training/src/components/Product.jsx
+++ react-training/src/components/Product.jsx
@@ -5,4 +5,11 @@ const Product = ({ name, imgSrc, width }) => (
   </div>
 );
 
+Product.defaultProps = {
+  name: "Produkt bez nazwy",
+  imgSrc:
+    "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
+  width: 150,
+};
+
 export default Product;
