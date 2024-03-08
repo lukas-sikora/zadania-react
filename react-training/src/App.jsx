855917bf0de4f1334b765ec1272b57b34b127049
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-08 19:11:54 +0100

    conditional-rendering-task/1

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,38 @@
-function App() {
-  return <div></div>
-}
+import { ProductDetails } from "./components/ProductDetails";
 
-export default App
+const product1 = {
+  name: "Laptop",
+  description: "Wysokiej klasy laptop do gier",
+  price: "4000 PLN",
+  isOnSale: true,
+};
+const product2 = {
+  name: "Słuchawki",
+  description: "Bezprzewodowe słuchawki z redukcją szumów",
+  price: "600 PLN",
+  isOnSale: false,
+};
+const product3 = {
+  name: "Smartfon",
+  description: "Nowoczesny smartfon z dużym ekranem",
+  price: "2500 PLN",
+  isOnSale: true,
+};
+const product4 = {
+  name: "Klawiatura",
+  description: "Mechaniczna klawiatura do gier",
+  price: "350 PLN",
+  isOnSale: false,
+};
+
+const App = () => {
+  return (
+    <>
+      <ProductDetails product={product1} />
+      <ProductDetails product={product2} />
+      <ProductDetails product={product3} />
+      <ProductDetails product={product4} />
+    </>
+  );
+};
+export default App;
