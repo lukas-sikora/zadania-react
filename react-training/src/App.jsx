b981e16d948a5c0f436646e914253c4f8fff23a9
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-08 20:53:19 +0100

    rendering-and-events-task/2

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,13 @@
-function App() {
-  return <div></div>
-}
+const App = () => {
+  return (
+    <>
+      <h1>Wybierz porę dnia:</h1>
+      <button>Rano</button>
+      <button>Popołudnie</button>
+      <button>Wieczór</button>
+      <div id="image">{/* Obrazek będzie się tutaj wyświetlał */}</div>
+    </>
+  );
+};
 
-export default App
+export default App;
