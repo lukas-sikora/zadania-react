c993dc0bf9b9998894cb32191bae1ddf26190926
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-08 21:48:34 +0100

    use-state-task/3

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,24 @@
-function App() {
-  return <div></div>
-}
+const App = () => {
+  return (
+    <>
+      <h1>Wspaniałe fakty o planetach</h1>
+      <button>Mars</button>
+      <button>Jowisz</button>
+      <button>Saturn</button>
+      <section>
+        <h2>Mars</h2>
+        <p>Mars jest znaną jako Czerwona Planeta.</p>
+      </section>
+      <section>
+        <h2>Jowisz</h2>
+        <p>Jowisz jest największą planetą w naszym układzie słonecznym.</p>
+      </section>
+      <section>
+        <h2>Saturn</h2>
+        <p>Saturn słynie z pięknych pierścieni.</p>
+      </section>
+    </>
+  );
+};
 
-export default App
+export default App;
