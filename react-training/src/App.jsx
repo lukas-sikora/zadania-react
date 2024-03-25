84418034294e67be7f049518df63360e2e59f0b8
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-25 16:27:46 +0100

    react-hook-form-task/1

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,28 @@
-function App() {
-  return <div></div>
-}
+import { useForm } from "react-hook-form";
 
-export default App
+const App = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("firstName", { required: true })}
+        placeholder="Imię"
+      />
+      <input
+        {...register("lastName", { required: true })}
+        placeholder="Nazwisko"
+      />
+      <input {...register("email", { required: true })} placeholder="Email" />
+      <input
+        {...register("password", { required: true })}
+        placeholder="Hasło"
+      />
+      <button type="submit">Zarejestruj</button>
+    </form>
+  );
+};
+
+export default App;
