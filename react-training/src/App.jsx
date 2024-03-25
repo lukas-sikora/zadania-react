072d01ecdd189563d7ac07ebebf76aec7e479c37
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-25 16:01:41 +0100

    forms-in-react-task/3

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,31 @@
-function App() {
-  return <div></div>
-}
+import { useState } from "react";
 
-export default App
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /* Tutaj dodaj walidację */
+    alert("Adres e-mail zapisany!");
+  };
+
+  return (
+    <>
+      <h1>Walidator adresu e-mail:</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            placeholder="Wpisz adres e-mail..."
+          />
+        </div>
+        <button>Wyślij</button>
+      </form>
+      <h2>{error}</h2>
+    </>
+  );
+};
+export default App;
