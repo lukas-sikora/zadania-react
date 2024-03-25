e0d1ec163e5b9328bfef1fc53ad35badcab07e32
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-25 16:15:00 +0100

    clean code

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,7 +1,57 @@
-import ColorPickerApp from "./components/ColorPickerApp";
+import { useState } from "react";
 
 const App = () => {
-  return <ColorPickerApp />;
-};
+  const [color, setColor] = useState("red"); // Ustawienie domyślnej wartości
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Nie jest potrzebne dodatkowe ustawienie stanu, ponieważ `color` już przechowuje wybrany kolor
+  };
+
+  return (
+    <>
+      <h1>Wybierz kolor:</h1>
+      <h2 style={{ backgroundColor: color }}>Wybrano kolor: {color}</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Czerwony
+            <input
+              type="radio"
+              name="color"
+              value="red"
+              checked={color === "red"}
+              onChange={(e) => setColor(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Zielony
+            <input
+              type="radio"
+              name="color"
+              value="green"
+              checked={color === "green"}
+              onChange={(e) => setColor(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Niebieski
+            <input
+              type="radio"
+              name="color"
+              value="blue"
+              checked={color === "blue"}
+              onChange={(e) => setColor(e.target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">Zapisz wybór</button>
+      </form>
+    </>
+  );
+};
 export default App;
