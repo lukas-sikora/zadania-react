fdd9bc29cbf1b7a5b46d4fbef417e89d6571d88d
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-25 16:12:06 +0100

    clean code

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,7 +1,32 @@
-import TaskApp from "./components/TaskApp";
+import { useState } from "react";
 
 const App = () => {
-  return <TaskApp />;
-};
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
 
+  const handleAddTask = (e) => {
+    e.preventDefault(); // Zapobiegamy domyślnej akcji przeglądarki
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  return (
+    <>
+      <h1>Lista zadań:</h1>
+      <form onSubmit={handleAddTask}>
+        <input
+          onChange={(e) => setTask(e.target.value)}
+          value={task}
+          placeholder="Dodaj nowe zadanie"
+        />
+        <button type="submit">Dodaj</button> {/* Zmieniamy typ na "submit" */}
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </form>
+    </>
+  );
+};
 export default App;
