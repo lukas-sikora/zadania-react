355738f6796c79ae255920b3853e1e59f0811c98
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-25 16:08:18 +0100

    forms-in-react-task/3-solution

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -4,10 +4,22 @@ const App = () => {
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
 
+  const validateEmail = (email) => {
+    const regex = /.+@.+/;
+    return regex.test(email);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    /* Tutaj dodaj walidację */
-    alert("Adres e-mail zapisany!");
+    if (validateEmail(email)) {
+      alert("Adres e-mail zapisany!");
+      setEmail("");
+      setError("");
+    } else {
+      setError(
+        "Niepoprawny adres e-mail. Upewnij się, że zawiera '@' oraz znaki przed i po nim."
+      );
+    }
   };
 
   return (
