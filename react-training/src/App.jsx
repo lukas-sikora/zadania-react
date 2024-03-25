d8c96baf9a57005510ca1db6ac11a56591d25440
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-25 16:50:37 +0100

    react-hook-form-task/2

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,52 @@
-function App() {
-  return <div></div>
-}
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export default App
+const App = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+
+  const onSubmit = async (data) => {
+    // Tutaj symulacja zapisu danych
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <div>
+          <label htmlFor="title">Tytuł</label>
+        </div>
+        <input
+          {...register("title")}
+          type="text"
+          id="title"
+          placeholder="Tytuł"
+        />
+      </div>
+      <div>
+        <div>
+          <label htmlFor="description">Opis</label>
+        </div>
+        <textarea
+          {...register("description")}
+          id="description"
+          placeholder="Opis"
+          rows={4}
+        />
+      </div>
+      <button type="submit" disabled={isSubmitting}>
+        Wyślij
+      </button>
+    </form>
+  );
+};
+
+export default App;
