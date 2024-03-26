7a670a4a23de587f2e725fa87733505dea0192e2
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-26 14:06:44 +0100

    css-modules-task/2-solution

--- react-training/src/components/ProductCard.jsx
+++ react-training/src/components/ProductCard.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import styles from "./productCard.module.css";
 
 const ProductCard = () => {
+  const [selectedSize, setSelectedSize] = useState("M");
+
+  const handleSizeChange = (e) => {
+    setSelectedSize(e.target.value);
+  };
+
   return (
     <div className={styles.card}>
       <h1 className={styles.title}>T-shirt "React"</h1>
-      <img
-        src="https://pngimg.com/uploads/tshirt/tshirt_PNG5454.png"
-        alt="T-shirt React"
-        className={"/* Jak zmienić klasę w zależności od rozmiaru? */"}
-      />
-      <select>
+      <div>
+        <img
+          src="https://pngimg.com/uploads/tshirt/tshirt_PNG5454.png"
+          alt="T-shirt React"
+          className={styles[`img--${selectedSize}`]}
+        />
+      </div>
+      <select onChange={handleSizeChange} value={selectedSize}>
         <option value="S">Small</option>
         <option value="M">Medium</option>
         <option value="L">Large</option>
