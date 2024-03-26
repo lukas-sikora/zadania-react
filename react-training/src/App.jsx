ddf8b8ffb3a7ff49002aea8821c7edfdd82ff2db
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-26 12:39:55 +0100

    import-css-task/1

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,38 @@
-function App() {
-  return <div></div>
-}
+import { Course } from "./components/Course";
 
-export default App
+const courses = [
+  {
+    key: "react",
+    name: "React",
+    description: "Biblioteka do budowania interfejsów użytkownika",
+    level: "Początkujący",
+  },
+  {
+    key: "angular",
+    name: "Angular",
+    description: "Platforma do budowania aplikacji internetowych",
+    level: "Zaawansowany",
+  },
+  {
+    key: "vue",
+    name: "Vue",
+    description: "Progresywny framework do tworzenia interfejsów użytkownika",
+    level: "Średniozaawansowany",
+  },
+];
+
+const App = () => {
+  return (
+    <>
+      <h1 className="title">Lista kursów programowania</h1>
+      <ul className="list">
+        {courses.map((course) => (
+          <li key={course.key}>
+            <Course course={course} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+export default App;
---
+++ react-training/src/components/Course.jsx
@@ -0,0 +1,10 @@
+const Course = ({ course }) => {
+  return (
+    <>
+      <h2 className="course-title">{course.name}</h2>
+      <p>{course.description}</p>
+      <span className="course-level">{course.level}</span>
+    </>
+  );
+};
+export default Course;
