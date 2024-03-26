11934e11e6a12ac6de65b9b6283fcdac223a629a
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-26 11:44:17 +0100

    arrays-in-react-task/1-solution

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
+
 const cities = [
-  { name: "Kraków", country: "Polska" },
-  { name: "Paryż", country: "Francja" },
-  { name: "Madryt", country: "Hiszpania" },
-  { name: "Berlin", country: "Niemcy" },
-  { name: "Rzym", country: "Włochy" },
+  { id: 1, name: "Kraków", country: "Polska" },
+  { id: 2, name: "Paryż", country: "Francja" },
+  { id: 3, name: "Madryt", country: "Hiszpania" },
+  { id: 4, name: "Berlin", country: "Niemcy" },
+  { id: 5, name: "Rzym", country: "Włochy" },
 ];
 
 const App = () => {
-  // Tu dodaj stan i logikę komponentu
+  const [selectedCityId, setSelectedCityId] = useState(cities[0].id);
+
+  const handleChange = (event) => {
+    setSelectedCityId(Number(event.target.value));
+  };
+
+  const selectedCityInfo = cities.find((city) => city.id === selectedCityId);
 
   return (
     <div>
       <h1>Wybierz miasto:</h1>
-      {/* Tu umieść element <select> oraz wyświetlanie wybranego miasta i kraju */}
+      <select value={selectedCityId} onChange={handleChange}>
+        {cities.map(({ id, name }) => (
+          <option key={id} value={id}>
+            {name}
+          </option>
+        ))}
+      </select>
+      <p>
+        Wybrałeś: {selectedCityInfo.name}, {selectedCityInfo.country}
+      </p>
     </div>
   );
 };
