cbc638289c778884e4e1129d969df5364a8fe413
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-26 10:41:36 +0100

    arrays-in-react-task/1

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-  return <div></div>
-}
+const cities = [
+  { name: "Kraków", country: "Polska" },
+  { name: "Paryż", country: "Francja" },
+  { name: "Madryt", country: "Hiszpania" },
+  { name: "Berlin", country: "Niemcy" },
+  { name: "Rzym", country: "Włochy" },
+];
 
-export default App
+const App = () => {
+  // Tu dodaj stan i logikę komponentu
+
+  return (
+    <div>
+      <h1>Wybierz miasto:</h1>
+      {/* Tu umieść element <select> oraz wyświetlanie wybranego miasta i kraju */}
+    </div>
+  );
+};
+export default App;
