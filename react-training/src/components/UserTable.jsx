368529b31a2e4a012148b1e06b9ef0b56f18c768
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-11 22:42:23 +0200

    use-effect-task/2-solution

--- react-training/src/components/UserTable.jsx
+++ react-training/src/components/UserTable.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 const usersData = [
   { id: 1, name: "Anna" },
   { id: 2, name: "Jan" },
@@ -5,9 +7,24 @@ const usersData = [
 ];
 
 const UsersTable = () => {
+  const [filter, setFilter] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState(usersData);
+
+  useEffect(() => {
+    const result = usersData.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+    setFilteredUsers(result);
+  }, [filter]);
+
   return (
     <div>
-      <input type="text" placeholder="Filtruj po imieniu..." />
+      <input
+        type="text"
+        placeholder="Filtruj po imieniu..."
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
       <table>
         <thead>
           <tr>
@@ -16,7 +33,7 @@ const UsersTable = () => {
           </tr>
         </thead>
         <tbody>
-          {usersData.map(({ id, name }) => (
+          {filteredUsers.map(({ name, id }) => (
             <tr key={id}>
               <td>{id}</td>
               <td>{name}</td>
