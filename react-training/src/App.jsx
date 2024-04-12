b42e655220fc4d42d2f1895d09806427a7872c88
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-12 09:41:25 +0200

    work-with-api-task/2-solution

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from "react";
+
 const PhotoBrowserApp = () => {
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetch(
+        "https://jsonplaceholder.typicode.com/photos?_limit=20"
+      );
+      const response = await data.json();
+      setPhotos(response);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (loading) {
+    return <p>Ładowanie zdjęć...</p>;
+  }
+
   return (
     <>
       <h1>Galeria zdjęć</h1>
@@ -9,7 +29,12 @@ const PhotoBrowserApp = () => {
           gap: "10px",
         }}
       >
-        Zdjęcia
+        {photos.map(({ id, url, title }) => (
+          <div key={id} style={{ marginBottom: "20px" }}>
+            <img src={url} alt={title} style={{ width: "100%" }} />
+            <p>Title: {title}</p>
+          </div>
+        ))}
       </div>
     </>
   );
