997fb1bb7e573c47af20230f590d420284c27136
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-12 07:04:51 +0200

    work-with-api-task/1

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,32 @@
+const posts = [
+  {
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    id: 1,
+  },
+  {
+    title: "qui est esse",
+    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+    id: 2,
+  },
+];
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Lista postów</h1>
+      <button>Odśwież</button>
+      <ul>
+        {posts.map(({ id, title, body }) => (
+          <li key={id}>
+            <h2>{title}</h2>
+            <p>{body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
