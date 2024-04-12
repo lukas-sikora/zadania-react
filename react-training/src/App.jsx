b9ada991614a8a7b13c836e4b063d26934da4404
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-12 10:45:16 +0200

    cleanup-effect-and-strict-mode-task/1

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,20 @@
-const App = () => {
-  return <div></div>;
-};
+import { useState } from "react";
 
+import Timer from "./components/Timer";
+
+const App = () => {
+  const [isActive, setIsActive] = useState(true);
+
+  const toggleTimer = () => {
+    setIsActive((prevIsActive) => !prevIsActive);
+  };
+
+  return (
+    <>
+      <h1>Zegar</h1>
+      <button onClick={toggleTimer}>{isActive ? "Zatrzymaj" : "Wznów"}</button>
+      {isActive && <Timer />}
+    </>
+  );
+};
 export default App;
---
+++ react-training/src/components/Timer.jsx
@@ -0,0 +1,4 @@
+const Timer = () => {
+  return <h2>10:12:44</h2>;
+};
+export default Timer;
