af6fe65d13055efcb4ccfb66904a740b3f800fbc
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-12 15:39:07 +0200

    context-in-react-task/3

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,24 @@
-const App = () => {
-  return <div></div>;
-};
+import { useState } from "react";
 
+import { Header, Body } from "./componets";
+
+const App = () => {
+  const [language, setLanguage] = useState("PL");
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <>
+      <select value={language} onChange={(e) => setLanguage(e.target.value)}>
+        <option value="PL">Polski</option>
+        <option value="EN">Angielski</option>
+      </select>
+      <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+        <option value="light">Jasny</option>
+        <option value="dark">Ciemny</option>
+      </select>
+      <Header />
+      <Body />
+    </>
+  );
+};
 export default App;
---
+++ react-training/src/componets/Body.jsx
@@ -0,0 +1,8 @@
+const Body = () => {
+  return (
+    <div>
+      <p>Tutaj możesz zmieniać ustawienia swojego profilu.</p>
+    </div>
+  );
+};
+export default Body;
