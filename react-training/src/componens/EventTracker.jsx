bc01de1b35e7662f8e5e81a6a4e02aed9ff1399c
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-12 11:47:42 +0200

    cleanup-effect-and-strict-mode-task/2-solution

--- react-training/src/componens/EventTracker.jsx
+++ react-training/src/componens/EventTracker.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+
 const EventTracker = () => {
-  return <div>Liczba kliknięć: 1</div>;
+  const [clicksCount, setClicksCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Dodanie nasłuchiwacza zdarzeń");
+    const handleClick = () => {
+      setClicksCount((prevCount) => prevCount + 1);
+    };
+
+    window.addEventListener("click", handleClick);
+    return () => {
+      console.log("Usunięcie nasłuchiwacza zdarzeń");
+      window.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  return <div>Liczba kliknięć: {clicksCount}</div>;
 };
 export default EventTracker;
