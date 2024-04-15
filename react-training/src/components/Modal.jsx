e8ac89775ed25fc9b9b4cee32c9f76369a91b2e0
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-15 10:44:19 +0200

    react-portal-task/2-solution

--- react-training/src/components/Modal.jsx
+++ react-training/src/components/Modal.jsx
@@ -1,5 +1,11 @@
 const Modal = ({ onClose }) => (
-  <div className="modal" onClick={onClose}>
+  <div
+    className="modal"
+    onClick={(e) => {
+      e.stopPropagation();
+      onClose();
+    }}
+  >
     <div className="modal-content">
       <p>Modal z komunikatem</p>
       <button onClick={onClose}>Zamknij</button>
