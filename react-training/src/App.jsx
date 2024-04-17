3bd4dfbe0ff051606190448aa8a58b88d3b571c3
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-17 16:05:55 +0200

    fix code

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,9 +1,15 @@
 const App = () => {
   const product = { name: "Smartfon XYZ", price: 2999 };
 
+  // element children
+  //   <h3>
+  //   Dostępność: <span>W magazynie</span>
+  //  </h3>
+
   return (
     <>
       <h1>Szczegóły produktu:</h1>
+      {/* W tym miejscu powinien znaleć się komponent ProductDetails*/}
       <hr />
       <h2>Ilość w koszyku: 2</h2>
       <button
