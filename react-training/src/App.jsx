a9f11241c0b1cb9f048ebead0b5d79de7e718306
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-17 12:48:27 +0200

    react-memo-task/1

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
+import { CountryStatistics } from "./components/CountryStatistics";
+
+const countries = [
+  { name: "Polska", population: 38000000, area: 312696 },
+  { name: "Niemcy", population: 83000000, area: 357022 },
+  { name: "Francja", population: 67000000, area: 640679 },
+];
+
 const App = () => {
-  return <div></div>;
+  const [selectedCountry, setSelectedCountry] = useState(countries[0]);
+
+  return (
+    <>
+      <h1>Statystyki krajów:</h1>
+      <select
+        onChange={(e) =>
+          setSelectedCountry(
+            countries.find(({ name }) => name === e.target.value)
+          )
+        }
+      >
+        {countries.map(({ name }) => (
+          <option key={name} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
+      <CountryStatistics country={selectedCountry} />
+    </>
+  );
 };
 
 export default App;
