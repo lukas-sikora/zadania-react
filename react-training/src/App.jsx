5c320cffe56e7669214efb6f1132cc773830be21
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-17 15:46:18 +0200

    react-memo-task/3

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,19 @@
 const App = () => {
-  return <div></div>;
-};
+  const product = { name: "Smartfon XYZ", price: 2999 };
 
+  return (
+    <>
+      <h1>Szczegóły produktu:</h1>
+      <hr />
+      <h2>Ilość w koszyku: 2</h2>
+      <button
+        onClick={() => {
+          "zmień stan produktu";
+        }}
+      >
+        Dodaj jeden produkt
+      </button>
+    </>
+  );
+};
 export default App;
