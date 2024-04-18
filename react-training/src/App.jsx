36c98393e7062200f6efb0fd7729fea63c4f1f07
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-18 07:24:09 +0200

    use-ref-task/1-solution

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,27 @@
+import { useState, useRef } from "react";
+
 const App = () => {
-  return <div></div>;
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const intervalRef = useRef(null);
+
+  const handleStart = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setCurrentNumber(Math.floor(Math.random() * 100) + 1);
+    }, 1000);
+  };
+
+  const handleStop = () => {
+    clearInterval(intervalRef.current);
+  };
+
+  return (
+    <>
+      <h1>Wylosowana liczba: {currentNumber}</h1>
+      <button onClick={handleStart}>Rozpocznij losowanie</button>
+      <button onClick={handleStop}>Zatrzymaj i pokaż wynik</button>
+    </>
+  );
 };
 
 export default App;
