354a75a8bf9300c13cd8aaa65ae789110894767f
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-18 09:10:08 +0200

    use-ref-task/3

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,18 @@
 const App = () => {
-  return <div></div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <h1>Stoper</h1>
+      <p>Pozostało sekund: 10</p>
+      <form onSubmit={handleSubmit}>
+        <input type="number" placeholder="Czas w sekundach" />
+        <button type="submit">Start</button>
+      </form>
+    </>
+  );
 };
 
 export default App;
