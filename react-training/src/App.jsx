101c1b1fe66602f28292f1fb86fada344cc1b160
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-18 07:19:30 +0200

    change kind of task

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,14 @@
 const App = () => {
-  return <div></div>;
+  const handleStart = () => {};
+  const handleStop = () => {};
+
+  return (
+    <>
+      <h1>Wylosowana liczba: 20</h1>
+      <button onClick={handleStart}>Rozpocznij losowanie</button>
+      <button onClick={handleStop}>Zatrzymaj i pokaż wynik</button>
+    </>
+  );
 };
 
 export default App;
