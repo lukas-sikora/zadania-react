d8de3960c43c8d013ec047d308b352777d54c7e7
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-04-18 10:21:13 +0200

    use-reducer-task/1-solution

--- react-training/src/components/TodoList.jsx
+++ react-training/src/components/TodoList.jsx
@@ -1,13 +1,47 @@
+import { useReducer, useState } from "react";
+
+const initialState = [];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      return [...state, { id: state.length, name: action.name }];
+    case "remove":
+      return state.filter(({ id }) => id !== action.id);
+    default:
+      return state;
+  }
+};
+
 const ToDoList = () => {
+  const [tasks, dispatch] = useReducer(reducer, initialState);
+  const [taskName, setTaskName] = useState("");
+
+  const addTask = () => {
+    if (taskName === "") return;
+
+    dispatch({ type: "add", name: taskName });
+    setTaskName("");
+  };
+
   return (
     <>
-      <input type="text" placeholder="Dodaj zadanie" />
-      <button>Dodaj</button>
+      <input
+        type="text"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+        placeholder="Dodaj zadanie"
+      />
+      <button onClick={addTask}>Dodaj</button>
       <ul>
-        <li>
-          Zrobić zakupy
-          <button>Usuń</button>
-        </li>
+        {tasks.map(({ id, name }) => (
+          <li key={id}>
+            {name}
+            <button onClick={() => dispatch({ type: "remove", id })}>
+              Usuń
+            </button>
+          </li>
+        ))}
       </ul>
     </>
   );
